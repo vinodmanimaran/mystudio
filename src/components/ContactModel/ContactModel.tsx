d25f43a9277c3d70cf669a29141fc035590f05b7
Vinod Kumar <vk5241415@gmail.com>
2024-03-06 10:59:06 +0530

    fixing ts errors v-1

--- src/components/ContactModel/ContactModel.tsx
+++ src/components/ContactModel/ContactModel.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import './ContactModel.css';
-import Lottie, { LottieOptions, EventListener, LottieRef } from 'react-lottie';
+import Lottie, { LottieProps, LottieRefOptions, AnimationEventCallback } from 'react-lottie';
 import Animation from '/public/Animation - 1709637307866.json';
 
-const defaultOptions: LottieOptions = {
+
+const defaultOptions: LottieProps['options'] = {
   loop: true,
   autoplay: true,
-  animationData: Animation,
+  animationData: Animation as any, // Cast animation data to any
   rendererSettings: {
     preserveAspectRatio: "xMidYMid slice"
   }
 }
 
 const ContactModel: React.FC = () => {
-  const lottieRef = React.useRef<LottieRef>(null);
+  const lottieRef = React.useRef<LottieRefOptions>(null);
 
-  const clickListener: EventListener = {
-    eventName: 'click',
-    callback: () => {} 
+  const completeListener: AnimationEventCallback = {
+    eventName: 'complete',
+    callback: () => {} // Empty callback to prevent complete event
   };
 
   return (
@@ -25,8 +26,8 @@ const ContactModel: React.FC = () => {
       <Lottie 
         options={defaultOptions}
         isClickToPauseDisabled={true}
-        eventListeners={[clickListener]}
-        ref={lottieRef}
+        eventListeners={[completeListener]}
+        lottieRef={lottieRef}
       />
     </div>
   );
