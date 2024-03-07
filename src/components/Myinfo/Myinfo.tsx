f9013ca23513ce8f3ef45b7aa3d8bf5a3bb9f80f
Vinod Kumar <vk5241415@gmail.com>
2024-03-07 21:55:45 +0530

    info-card

--- src/components/Myinfo/Myinfo.tsx
+++ src/components/Myinfo/Myinfo.tsx
@@ -6,8 +6,8 @@ import callIcon from '../../assets/icons/contact.svg';
 
 const MyInfoCard: React.FC = () => {
   return (
-    <div className="">
-      <div className="w-full relative max-w-xs myinfo-card ">
+    <div className="myinfo-card ">
+      <div className=" relative  detailcard">
         <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
                 <div className="relative shadow-xl bg-gray-900 border border-gray-800 px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
           <h1 className="font-bold text-xl text-white mb-4 relative z-50  company-name">
